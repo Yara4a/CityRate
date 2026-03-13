32cfc064b626f7bdf49c55978ad79a4df4818103
Rabindra Sunuwar <3040215s@student.gla.ac.uk>
2026-03-13 05:23:35 +0000

    Finished Week 1 auth and Week 2 email validation

--- city/forms.py
+++ city/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Post
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 
 class PostForm(forms.ModelForm):
@@ -14,3 +16,15 @@ class PostForm(forms.ModelForm):
             raise forms.ValidationError("Rating must be between 1 and 5.")
 
         return rating
+    
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True, help_text="Required for validation.")
+
+    class Meta(UserCreationForm.Meta):
+        fields = UserCreationForm.Meta.fields + ("email",)
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("A user with this email already exists.")
+        return email
