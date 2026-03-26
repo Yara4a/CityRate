c05edcd991444984bb7887d39318a9a6a658bc40
Yara Alshetaiwi <3075453a@student.gla.ac.uk>
2026-03-26 21:08:33 +0000

    Add root redirect to homepage

--- cityRate/urls.py
+++ cityRate/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
 
 urlpatterns = [
+    path("", lambda request: redirect("home")),
     path("admin/", admin.site.urls),
     path("cityrate/", include("city.urls")),
 ]
